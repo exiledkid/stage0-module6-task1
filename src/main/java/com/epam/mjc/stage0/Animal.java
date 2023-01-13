2188be11de9b4e1372ec7caf95e164d1a81b9144
exiledkid <fortunemodspartner@gmail.com>
2023-01-14 02:49:29 +0300

    )))

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String pawsAmount = numberOfPaws == 1 ? " paw " : " paws ";
+        String fur = hasFur ? " a " : " no ";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + pawsAmount + "and" + fur + "fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird () {
+        super("blue", 2, false);
+    }
+    @Override
 
+    public String getDescription() {
+
+        String firstPart = super.getDescription();
+        return firstPart + " Moreover, it has 2 wings and can fly.";
+    }
 }
